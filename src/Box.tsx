78a7941d19ef402281b7852786c0d1825567d3b2
Max Lascombe <mtl2154@columbia.edu>
2023-01-31 09:38:12 -0500

    Added click styling to boxes.

--- src/Box.tsx
+++ src/Box.tsx
@@ -38,7 +38,7 @@ const Box = ({
         <div
             style={{ height: height + 6, left, top, width }}
             className={
-                'flex flex-col items-center justify-center absolute bg-black border-2 border-b-8 border-white rounded-xl p-2 text-sm text-white inline-block text-center ' +
+                'flex flex-col items-center justify-center absolute bg-black border-2 border-b-8 active:border-b-2 active:mt-0.5 border-white rounded-xl p-2 text-sm text-white inline-block text-center ' +
                 className
             }>
             {children}
