62d5bfcefddae888558bdaf3420c5b0b99ee3883
Max Lascombe <mtl2154@columbia.edu>
2024-03-17 10:18:44 +0800

    Updated progress.

--- src/progress/goals.tsx
+++ src/progress/goals.tsx
@@ -19,13 +19,13 @@ export const goals: Record<
     goals: [
       ['Publish 42 creations (including newsletters)', 2, 42],
       ['Read 25 books', 2, 25],
-      ['Meditate 250 times', 47, 250],
-      ['Do 36,500 pushups', 6_354, 36_500],
-      ['Do 100 endurance workouts', 13, 100],
+      ['Meditate 250 times', 52, 250],
+      ['Do 36,500 pushups', 6_970, 36_500],
+      ['Do 100 endurance workouts', 15, 100],
       ['Check seven items off my bucket list', 0, 7],
       ['Travel to five different countries', 3, 5],
       ['Have a conversation in Tagalog', 0, 1],
-      ['Complete all 16 of my work milestones', 1, 16],
+      ['Complete all 16 of my work milestones', 2, 16],
     ],
   },
 }
