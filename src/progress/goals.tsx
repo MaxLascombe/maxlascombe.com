2177955cb694cf4a65dee124b60d707960581ff6
Max Lascombe <MaxLascombe@users.noreply.github.com>
2024-11-24 13:25:13 +0800

    Update goals.tsx

--- src/progress/goals.tsx
+++ src/progress/goals.tsx
@@ -33,10 +33,10 @@ export const goals: Record<
   2024: {
     link: 'https://lifetothemax.substack.com/p/2024-goals',
     goals: [
-      ['Publish 42 creations (including newsletters)', 20, 42],
-      ['Read 25 books', 19, 25],
-      ['Meditate 250 times', 226, 250],
-      ['Do 36,500 pushups', 16_364, 36_500],
+      ['Publish 42 creations (including newsletters)', 22, 42],
+      ['Read 25 books', 20, 25],
+      ['Meditate 250 times', 230, 250],
+      ['Do 36,500 pushups', 16_789, 36_500],
       ['Do 100 endurance workouts', 60, 100],
       [
         'Check seven items off my bucket list',
