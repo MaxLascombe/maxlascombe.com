dafa57189681d3ceea307c10f7f9438d0bcbe581
Max Lascombe <MaxLascombe@users.noreply.github.com>
2025-02-09 11:18:08 +0800

    Update goals.tsx

--- src/progress/goals.tsx
+++ src/progress/goals.tsx
@@ -53,9 +53,9 @@ export const goals: Record<
     goals: [
       ['Publish 20 posts on lifetothemax', 1, 20],
       ['Publish 10 other creations', 0, 10],
-      ['Read 25 books', 1, 25],
-      ['Meditate 200 times', 9, 200],
-      ['Watch 100 movies', 9, 100],
+      ['Read 25 books', 2, 25],
+      ['Meditate 200 times', 10, 200],
+      ['Watch 100 movies', 13, 100],
       ['Spend 0 minutes on Twitter or TikTok', 1, 1],
       ['Learn how to lift weights', 0, 1],
       ['Train for and complete a 5k, ...', 0, 1],
