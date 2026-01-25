f4b5083815179883896cd2bf40548877996eaf54
Max Lascombe <MaxLascombe@users.noreply.github.com>
2026-01-26 00:48:07 +0800

    Update goals.tsx

--- src/progress/goals.tsx
+++ src/progress/goals.tsx
@@ -74,7 +74,7 @@ export const goals: Record<
   2026: {
     link: '',
     goals: [
-      ['Build and maintain a 30-day running streak', 31, 30],
+      ['Build and maintain a 30-day running streak', 38, 30],
       ['Read 25 books', 1, 25],
       ['Quit all algorithmic feeds', 1, 1],
       ['End the year with more money than I started', 0, 1],
